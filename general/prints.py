3d401b623eb1b1445a15306e99cd4a4b7224105d
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2017-04-20 07:09:52 +1000

    debug print report with nginx

--- general/prints.py
+++ general/prints.py
@@ -212,7 +212,7 @@ def print_report(request):
     log.debug(benefits)
     log.debug(plans)
     log.debug('@@@@@@@@@@@2')
-    return get_pdf(request, benefits[-5:], plans[-5:])
+    return get_pdf(request, benefits[-6:], plans[-6:])
 
 
 def get_download_response(path):
